53a727eca59e86b58b2b0184d98b61003869eee6
Divy Goyal <86590133+divygoyal@users.noreply.github.com>
2023-08-17 01:04:32 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -164,8 +164,8 @@ const[sortarr , setsortarr] = useState([]);
         <div className="recipe">
         <div className="recipeimage">
         <Link to={`/recipe/${object._id}`}>
-        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/${object.imageurl}`} alt="" /> }
-        {object.image && <img src={`https://recipeapi-apm6.onrender.com/${object.image}`} alt="" /> }
+        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.imageurl}`} alt="" /> }
+        {object.image && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.image}`} alt="" /> }
         
         </Link>
         
@@ -222,7 +222,7 @@ const[sortarr , setsortarr] = useState([]);
         <div className="recipeimage">
         <Link to={`/recipe/${object._id}`}>
         {/* <img src={`http://localhost:3000/uploads/${object.image}`} alt="" /> */}
-        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/${object.imageurl}`} alt="" /> }
+        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.imageurl}`} alt="" /> }
         {object.image && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.image}`} alt="" /> }
         </Link>
         </div>
@@ -246,8 +246,8 @@ const[sortarr , setsortarr] = useState([]);
         <div className="recipeimage">
         <Link to={`/recipe/${object._id}`}>
         {/* <img src={`http://localhost:3000/uploads/${object.image}`} alt="" /> */}
-        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/${object.imageurl}`} alt="" /> }
-        {object.image && <img src={`https://recipeapi-apm6.onrender.com/${object.image}`} alt="" /> }
+        {object.imageurl && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.imageurl}`} alt="" /> }
+        {object.image && <img src={`https://recipeapi-apm6.onrender.com/uploads/${object.image}`} alt="" /> }
         </Link>
         </div>
         <p style={{marginTop:"15px"}}>{object.name}</p>
